c9bc082f48a452a0fba1e0e2cef1bca4f0252b3c
patsalcedo <patricia.salcedo15@outlook.com>
2021-12-01 16:31:52 +1300

    Changed appbar title to Scrapz logo

--- lib/components/appbar.dart
+++ lib/components/appbar.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:scrapz/components/custom_shape.dart';
+import 'package:scrapz/themes/scrapz_theme.dart';
 
 class CustomAppBar extends StatelessWidget with PreferredSizeWidget {
   final String title;
@@ -16,18 +17,31 @@ class CustomAppBar extends StatelessWidget with PreferredSizeWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          toolbarHeight: 1500,
+          toolbarHeight: 2000,
           backgroundColor: Colors.transparent,
           elevation: 0.0,
           flexibleSpace: ClipPath(
             clipper: CustomShape(),
             child: Container(
-              height: 500,
+              height: 1000,
               width: MediaQuery.of(context).size.width,
-              color: Colors.deepOrange,
+              color: scrapzOrange,
               child: Center(
-                child: Text('Scrapz',
-                    style: TextStyle(fontSize: 25, color: Colors.white)),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      "assets/logo/logo_white_carrot.png",
+                      fit: BoxFit.contain,
+                      height: 20,
+                    ),
+                    Image.asset(
+                      "assets/logo/logo_white_name.png",
+                      fit: BoxFit.contain,
+                      height: 20,
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
